cfe14054584234a89e6815f49008c69c89a99d1d
Joseph Guzman <joseph06.06.96@gmail.com>
2021-06-08 22:18:06 -0500

    Agregué función de registro de Cuentas

--- PrimeraSesion/src/PrimeraSesion/PruebaCuenta.java
+++ PrimeraSesion/src/PrimeraSesion/PruebaCuenta.java
@@ -1,49 +1,45 @@
 package PrimeraSesion;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class PruebaCuenta {
 
-    public static void main (String[] args) throws java.lang.Exception
+    public static ArrayList<Cuenta> registrarClientes() {
+        //Lista de clientes(cuentas)
+        ArrayList<Cuenta> clientes = new ArrayList<>();
+        Scanner registra = new Scanner(System.in);
+        String opcion="y";
+        while(!opcion.equals("N")){
+            Cuenta auxCliente = new Cuenta();
+            System.out.println("Ingresa nombre");
+            String nombre = registra.nextLine();
+            auxCliente.setNombre(nombre);
+            System.out.println("Ingresa saldo");
+            int saldo = registra.nextInt();
+            registra.nextLine();
+            auxCliente.setSaldo(saldo);
+            clientes.add(auxCliente);
+            System.out.println("Ingresar nueva cuenta? [y/N]");
+            opcion = registra.nextLine();
+        }
+        return clientes;
+    }
+
+    public static void main (String[] args)
     {
+        ArrayList<Cuenta> clientes = registrarClientes();
         //creacion del escaner
         Scanner entrada = new Scanner(System.in);
 
-        //creacion del objeto cuenta
-        Cuenta miCuenta = new Cuenta();
+        System.out.println("Ingresar la cuenta de donde se desea retirar (Existen disponibles "
+                            + clientes.size()
+                            + " clientes):");
+        int index = entrada.nextInt();
+        index++;
+        System.out.println("Ingresar el monto a retirar: ");
+        int monto = entrada.nextInt();
 
-        //muestra que en el objeto aun no hay datos, null
-        System.out.printf("El nombre inicial es: %s %n %n", miCuenta.getNombre());
-
-        //pide y lee el nombre
-        System.out.println("Introduce el nombre de la cuenta: ");
-        String elnombre = entrada.nextLine();
-        miCuenta.setNombre(elnombre);
-        System.out.println();
-
-        //muestra el nombre de la cuenta
-        System.out.printf("El nombre en el objeto miCuenta es: %n %s %n", miCuenta.getNombre());
-
-        //pide y lee el saldo de la cuenta
-        System.out.println("Ingrese el saldo de la cuenta: ");
-        float saldo= entrada.nextFloat();
-
-        //establece el saldo del objeto creado
-        miCuenta.setSaldo(saldo);
-
-        //pide y lee el monto a retirar
-        System.out.println("Ingrese el monto a retirar: ");
-        float monto= entrada.nextFloat();
-
-        //retira el monto del saldo actual, y evalua si es posible realziar la operacion
-        miCuenta.Retirar(monto);
-
-        System.out.println("");
-
-        //otro ejemplo de prueba usando el constructor con parametros
-        Cuenta cuentaPrueba = new Cuenta ("Ruby", 1000);
-
-        //realiza el retiro de la cuenta de prueba
-        cuentaPrueba.Retirar(1500);
+        clientes.get(index).Retirar(monto);
     }
 }
 
