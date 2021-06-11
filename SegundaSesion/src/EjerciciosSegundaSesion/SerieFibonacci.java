e345681d37a99a995ed7e785d3188cb48769a471
RUBYsheyla18 <sheylarubyzq@gmail.com>
2021-06-11 00:14:43 -0500

    SegundaSesion

--- SegundaSesion/src/EjerciciosSegundaSesion/SerieFibonacci.java
+++ SegundaSesion/src/EjerciciosSegundaSesion/SerieFibonacci.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 public class SerieFibonacci {
     public static int nesimoFibonacci(int nesimo){
-        //creacion de un arreglo para facilitar el calculo de la serie de fibonacci
-        int[] fibonacci = new int[nesimo];
 
-        //inicializacion de los primeros valores de la serie de fibanacci
-        fibonacci[0]=0;
-        fibonacci[1]=1;
+        switch (nesimo){
+            case 1:
+                return 0;
+            case 2:
+                return 1;
+            default:
+            {
+                //creacion de un arreglo para facilitar el calculo de la serie de fibonacci
+                int[] fibonacci = new int[nesimo];
 
-        //calculo del n-esimo numero de la serie de fibonacci
-        for(int i = 2;i<fibonacci.length;i++)
-        {
-            fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+                //inicializacion de los primeros valores de la serie de fibanacci
+                fibonacci[0]=0;
+                fibonacci[1]=1;
+                //calculo del n-esimo numero de la serie de fibonacci
+                for(int i = 2;i< fibonacci.length;i++)
+                {
+                    fibonacci[i] = fibonacci[i-2] + fibonacci[i-1];
+                }
+
+                return fibonacci[fibonacci.length - 1];
+            }
         }
-
-        return fibonacci[fibonacci.length];
 
     }
 
@@ -34,6 +43,12 @@ public class SerieFibonacci {
             System.out.println("Ingrese el numero: ");
             int nesimo = lectura.nextInt();
 
+            //verifica que el valor ingresado, el cual debe de ser mayor a cero
+            while (nesimo <= 0) {
+                System.out.println("Error, numero ingresado debe de ser mayor a 0");
+                System.out.println("Ingrese el numero: ");
+                nesimo = lectura.nextInt();
+            }
             //realizamos el calculo del n-esimo numero de la serie de fibonacci en base al numero ingresado
             System.out.printf("El n-esimo numero de la serie de Fibonacci es: %n%d%n",nesimoFibonacci(nesimo));
 
--- SegundaSesion/src/EjerciciosSegundaSesion/TableroDamas.java
+++ SegundaSesion/src/EjerciciosSegundaSesion/TableroDamas.java
@@ -1,5 +1,34 @@
 package EjerciciosSegundaSesion;
 //TableroDamas.java
-//imprime un tablero de damas
+//imprime un tablero de damas de 8 x 8
 public class TableroDamas {
+    public static void main(String[] args) {
+        //se inicializa el valor del numero de linea, dado que esta variable
+        //indicara si la linea sera par o impar para cambiar el patron de impresion de la cadena de texto
+        int numeroLinea = 0;
+
+        //mediante un ciclo for, se repetira 8 veces para poder imprimir las 8 filas del tablero
+        for(int i= 0; i< 8; i++){
+            //se verifica si numeroLinea es par o impar para poder imprimir el patron del tablero
+            if(numeroLinea%2 == 0){
+                //en caso sea par, se empezara por imprimir * 8 veces
+                for(int j = 0; j<8; j++){
+                    System.out.print("* ");
+                }
+                //se incrementa el numero de linea
+                numeroLinea++;
+            }else{
+                //en caso sea impar, se imprimira primero un espacio de linea
+                //luego seguido por 8 *'s
+                System.out.print(" ");
+                for(int j = 0; j<8; j++){
+                    System.out.print("* ");
+                }
+                //se incrementa el numero de linea
+                numeroLinea++;
+            }
+            //se realiza el salto de linea para imprimir la sgt linea
+            System.out.println();
+        }
+    }
 }
